c7051e0bead81ec50d7a2afd293e1b81ea499de7
Charlie <itscharlieliu@gmail.com>
2021-03-13 11:04:34 -0800

    Add ability to read multiple files

--- cat.go
+++ cat.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
+	"os"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFile(filename string) {
+	file, err := os.Open(filename)
+	check(err)
+
+	for {
+		// Create bytes slice of size 8
+		bytesSlice := make([]byte, 8)
+		// Read file up to len(bytesSlice) bytes
+		numRead, err := file.Read(bytesSlice)
+		if err != nil {
+			// Either there was an error or we reached EOF
+			break
+		}
+		fmt.Printf("%s", string(bytesSlice[:numRead]))
+	}
+}
+
 func main() {
-	fmt.Println(quote.Go())
+
+	args := os.Args[1:]
+
+	for i := 0; i < len(args); i++ {
+		readFile(args[i])
+	}
+
 }
