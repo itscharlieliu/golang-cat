398ecb927053b7cb08562f4bea397e3d18b85f27
Charlie <itscharlieliu@gmail.com>
2021-03-17 17:14:45 -0700

    Fix test for cat

--- test/cat_test.go
+++ test/cat_test.go
@@ -1,6 +1,9 @@
 package test
 
 import (
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/itscharlieliu/golang-cat/pkg"
@@ -8,9 +11,27 @@ import (
 
 func TestCat(t *testing.T) {
 
-	commandOutputString := pkg.GetStdout(func() { main.ReadFile("test.txt") })
+	commandOutputString := pkg.GetStdout(func() { pkg.ReadFile("test.txt") })
 
-	if commandOutputString != "test" {
-		t.Errorf("Expected %s, got %s", "test", commandOutputString)
+	exe, _ := exec.LookPath("cat")
+
+	reader, writer, _ := os.Pipe()
+
+	cmd := &exec.Cmd{
+		Path:   exe,
+		Args:   []string{exe, "test.txt"},
+		Stdout: writer,
+		Stderr: writer,
+	}
+
+	cmd.Run()
+
+	writer.Close()
+	output, _ := ioutil.ReadAll(reader)
+
+	realOutputString := string(output)
+
+	if commandOutputString != realOutputString {
+		t.Errorf("Expected %s, got %s", realOutputString, commandOutputString)
 	}
 }
