d62cc4600c87d1734e3c03e0e02974147203d7b4
Charlie <itscharlieliu@gmail.com>
2021-03-19 18:01:46 -0700

    Move run command into its own function

--- test/cat_test.go
+++ test/cat_test.go
@@ -1,37 +1,45 @@
 package test
 
 import (
-	"io/ioutil"
-	"os"
-	"os/exec"
 	"testing"
 
 	"github.com/itscharlieliu/golang-cat/pkg"
 )
 
-func TestCat(t *testing.T) {
+func TestCatNoArgs(t *testing.T) {
 
 	commandOutputString := pkg.GetStdout(func() { pkg.ReadFile("test.txt") })
 
-	exe, _ := exec.LookPath("cat")
-
-	reader, writer, _ := os.Pipe()
-
-	cmd := &exec.Cmd{
-		Path:   exe,
-		Args:   []string{exe, "test.txt"},
-		Stdout: writer,
-		Stderr: writer,
-	}
-
-	cmd.Run()
-
-	writer.Close()
-	output, _ := ioutil.ReadAll(reader)
-
-	realOutputString := string(output)
+	realOutputString := pkg.RunCmd("cat", []string{"test.txt"})
 
 	if commandOutputString != realOutputString {
 		t.Errorf("Expected %s, got %s", realOutputString, commandOutputString)
 	}
 }
+
+// func TestCatMultipleFiles(t *testing.T) {
+
+// 	commandOutputString := pkg.GetStdout(func() { pkg.ReadFile("test.txt") })
+
+// 	exe, _ := exec.LookPath("cat")
+
+// 	reader, writer, _ := os.Pipe()
+
+// 	cmd := &exec.Cmd{
+// 		Path:   exe,
+// 		Args:   []string{exe, "test.txt"},
+// 		Stdout: writer,
+// 		Stderr: writer,
+// 	}
+
+// 	cmd.Run()
+
+// 	writer.Close()
+// 	output, _ := ioutil.ReadAll(reader)
+
+// 	realOutputString := string(output)
+
+// 	if commandOutputString != realOutputString {
+// 		t.Errorf("Expected %s, got %s", realOutputString, commandOutputString)
+// 	}
+// }
