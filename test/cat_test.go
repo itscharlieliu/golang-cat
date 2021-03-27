df9addfc3ce3a6155ed6ba4b4051dd763a068ecb
Charlie <itscharlieliu@gmail.com>
2021-03-26 17:19:33 -0700

    Add test for creating a new file

--- test/cat_test.go
+++ test/cat_test.go
@@ -8,7 +8,6 @@ import (
 
 func TestCatNoArgs(t *testing.T) {
 
-	// commandOutputString := pkg.GetStdout(func() { pkg.ReadFile("test.txt") })
 	commandOutputString := pkg.RunCmd("go", []string{"run", "../cmd/cat.go", "test.txt"})
 
 	realOutputString := pkg.RunCmd("cat", []string{"test.txt"})
@@ -20,7 +19,6 @@ func TestCatNoArgs(t *testing.T) {
 
 func TestCatMultipleFiles(t *testing.T) {
 
-	// commandOutputString := pkg.GetStdout(func() { pkg.ReadFile("test.txt") })
 	commandOutputString := pkg.RunCmd("go", []string{"run", "../cmd/cat.go", "test.txt", "test2.txt"})
 
 	realOutputString := pkg.RunCmd("cat", []string{"test.txt", "test2.txt"})
@@ -32,12 +30,24 @@ func TestCatMultipleFiles(t *testing.T) {
 
 func TestCatLineNumbers(t *testing.T) {
 
-	// commandOutputString := pkg.GetStdout(func() { pkg.ReadFile("test.txt") })
 	commandOutputString := pkg.RunCmd("go", []string{"run", "../cmd/cat.go", "-n", "test.txt"})
 
 	realOutputString := pkg.RunCmd("cat", []string{"-n", "test.txt"})
 
 	if commandOutputString != realOutputString {
 		t.Errorf("Expected: \n%s\nGot \n%s\n", realOutputString, commandOutputString)
+	}
+}
+
+func TestCatCreateNewFile(t *testing.T) {
+	pkg.RunCmd("go", []string{"run", "../cmd/cat.go", "test.txt", ">", "newfile.txt"})
+
+	pkg.RunCmd("cat", []string{"test.txt", ">", "newfile2.txt"})
+
+	text1 := pkg.RunCmd("cat", []string{"newfile.txt"})
+	text2 := pkg.RunCmd("cat", []string{"newfile2.txt"})
+
+	if text1 != text2 {
+		t.Errorf("Expected: \n%s\nGot \n%s\n", text2, text1)
 	}
 }
